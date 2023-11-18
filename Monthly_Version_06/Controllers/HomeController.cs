2c68bad694480c0866346c316c8ada571d4e9ad3
Jahangir Alam <ja3560124@gmail.com>
2023-11-18 13:50:41 +0600

    2

---
+++ Monthly_Version_06/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Monthly_Version_06.Models;
+using System.Diagnostics;
+
+namespace Monthly_Version_06.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Chat()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Monthly_Version_06/Hubs/MessageHub.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Monthly_Version_06.Hubs
+{
+    public class MessageHub : Hub
+    {
+        static Dictionary<string, string> users = new Dictionary<string, string>();
+        private readonly IWebHostEnvironment env;
+        public MessageHub(IWebHostEnvironment env)
+        {
+            this.env = env;
+        }
+
+        public async override Task OnConnectedAsync()
+        {
+            await Clients.Caller.SendAsync("connection", "You are connected");
+        }
+
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        public async Task JoinAsync(string username)
+        {
+            users.Add(Context.ConnectionId, username);
+            await Clients.All.SendAsync("userList", users.Values.ToList());
+        }
+
+        public async Task SendAllAsync(string msg)
+        {
+            await Clients.All.SendAsync("message", new { sender = users[Context.ConnectionId], message = msg });
+        }
+
+        public async Task UploadAsync(FileData data)
+        {
+            string ext = Path.GetExtension(data.Filename);
+            string fileName = Path.GetFileNameWithoutExtension(Path.GetRandomFileName()) + ext;
+            string savePath = Path.Combine(this.env.WebRootPath, "Uploads", fileName);
+            byte[] buffer = Convert.FromBase64String(data.Content.Split(',')[1]);
+            await File.WriteAllBytesAsync(savePath, buffer);
+            await Clients.All.SendAsync("uploaded", new { sender = users[Context.ConnectionId], File = fileName, Type = ext });
+        }
+    }
+
+    public class FileData
+    {
+        public string Filename { get; set; } = default!;
+        public string Content { get; set; } = default!;
+    }
+}
